88c4e842cc38d37d176960ea25d6ce62056c95be
BangMingGing <privatebmk802@naver.com>
2023-06-04 00:02:50 +0900

    add for drone without Logger

---
+++ Task_Manager2.py
@@ -0,0 +1,62 @@
+import argparse
+import pika
+import pickle
+import time
+
+from Vehicle import Controller
+
+RABBITMQ_SERVER_IP = '203.255.57.129'
+RABBITMQ_SERVER_PORT = '5672'
+
+
+class Task_Consumer():
+    
+    def __init__(self, controller, drone_name):
+        self.credentials = pika.PlainCredentials('rabbitmq', '1q2w3e4r')
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_SERVER_IP, RABBITMQ_SERVER_PORT, 'vhost', self.credentials))
+        self.channel = self.connection.channel()
+        
+        self.my_name = '[Task_Consumer]'
+        
+        self.drone_name = drone_name
+        self.queue_name = drone_name
+        self.exchange_name = 'input'
+
+        # Queue 선언
+        queue = self.channel.queue_declare(self.queue_name)
+        # Queue-Exchange Binding
+        self.channel.queue_bind(exchange=self.exchange_name, queue=self.queue_name, routing_key=f'to{self.queue_name}')
+
+        # Controller 인스턴스 생성
+        self.controller = controller
+        
+    
+    def callback(self, ch, method, properties, body):
+        message = pickle.loads(body, encoding='bytes')
+        header = message['header']
+        contents = message['contents']
+        
+        self.controller.control(header, contents)
+        
+        
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+    def consume(self):
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(on_message_callback=self.callback, queue=self.queue_name)
+        print(f"{self.my_name} Start Consuming")
+        self.channel.start_consuming()
+        
+        
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--drone_name', help='drone_name argument')
+    
+    args, unknown = parser.parse_known_args()
+    
+    drone_name = args.drone_name
+    
+    process = Task_Consumer(drone_name)
+    process.consume()
---
+++ main2.py
@@ -0,0 +1,39 @@
+import argparse
+
+from threading import Thread, Lock
+
+from Task_Manager2 import Task_Consumer
+from Monitoring_System2 import Logging_Publisher
+from Vehicle2 import Controller
+
+
+def task_consumer(controller, drone_name):
+    process = Task_Consumer(controller, drone_name)
+    process.consume()
+    
+def logger(controller, drone_name):
+    process = Logging_Publisher(controller, drone_name)
+    process.Logging()
+    
+    
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--drone_name', help='drone_name argument')
+    
+    args, unknown = parser.parse_known_args()
+    
+    drone_name = args.drone_name
+    
+    controller = Controller(drone_name)
+
+    th1 = Thread(target=task_consumer, args=(controller, drone_name, ))
+    th2 = Thread(target=logger, args=(controller, drone_name, ))
+    
+    th1.start()
+    th2.start()
+    
+    th1.join()
+    th2.join()
+    
+    print('end')
