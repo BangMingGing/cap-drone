f9ba273d7cbaac01aa6d0a4853ee453d58da2b98
BangMingGing <privatebmk802@naver.com>
2023-08-01 02:23:09 +0000

    remove origin version

--- Monitoring_System.py
+++ Monitoring_System.py
@@ -12,7 +12,7 @@ RABBITMQ_SERVER_PORT = '5672'
 
 class Logging_Publisher():
     
-    def __init__(self, drone_name, lock):
+    def __init__(self, controller, drone_name):
         self.credentials = pika.PlainCredentials('rabbitmq', '1q2w3e4r')
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_SERVER_IP, RABBITMQ_SERVER_PORT, 'vhost', self.credentials))
         self.channel = self.connection.channel()
@@ -24,7 +24,7 @@ class Logging_Publisher():
         self.channel.exchange_declare(exchange='monitoring', exchange_type='direct')
 
         # Logger 인스턴스 생성
-        self.logger = Logger(drone_name, lock)
+        self.controller = controller
         
         self.term = 1
 
@@ -40,10 +40,10 @@ class Logging_Publisher():
     
     
     def Logging(self):
-        print(f"{self.my_name} Start Logging")
+        print(f"{self.my_name} Start Logging _ {self.drone_name}")
         while True:
             try:
-                GPS_info = asyncio.run(self.logger.get_status())
+                GPS_info = self.controller.GPS
                 create_time = time.time()
                 message = {'create_at': create_time, 'drone_name': self.drone_name, 'GPS_info': GPS_info}
                 self.publish(message, 'monitoring', 'tolog_queue')
